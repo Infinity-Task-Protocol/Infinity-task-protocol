ba09654a4fa186f74ba7bcccc2b98b74347b01e7
Rbto-flores <roberto.floresrosas@pm.me>
2025-07-06 22:17:00 -0400

    delete libs

--- src/frontend/app/app.vue
+++ src/frontend/app/app.vue
@@ -1,12 +1,20 @@
 <script setup lang="ts">
-import "@/assets/libs/@mdi/font/css/materialdesignicons.min.css";
+
 import '@/assets/libs/@iconscout/unicons/css/line.css'
 </script>
 
 <template>
 
-    <NuxtLayout>
-        <NuxtPage />
-    </NuxtLayout>
+  <NuxtLayout>
+    <NuxtPage/>
+  </NuxtLayout>
 
 </template>
+
+<Head>
+<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+<link
+    rel="stylesheet"
+    href="https://unicons.iconscout.com/release/v4.0.0/css/unicons.css"
+/>
+</Head>
